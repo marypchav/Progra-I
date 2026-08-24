0aac84e93c77474007391aa656dc747adf2b4bb2
mary <131309736+marypchav@users.noreply.github.com>
2026-08-24 16:53:34 -0600

    Cambio a la función "mostrarTabla()", ahora llama al SP con la tabla ordenada de forma alfabética

--- prueba.js
+++ prueba.js
@@ -1,25 +1,7 @@
-/*
-const http = require('node:http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-// Create the server and define the response
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('holaaa!\n');
-});
-
-// Start listening for requests
-server.listen(port, hostname, () => {
-  console.log(`aquí está tu server bella un abrazo http://${hostname}:${port}/`);
-});
-*/
+// CONSTANTES
 
 const sql = require('mssql');
 const { default: Null } = require('tedious/lib/data-types/null');
-
 const bdSettings = {
   user: 'sa',
   password: 'basesI',
@@ -31,30 +13,28 @@ const bdSettings = {
   }
 };
 
-async function mostrarTabla(id = null) {
+// FUNCIONES UI
+
+// obtener tabla
+async function obtenerTabla() {
+  //la renombré a "obtenerTabla" para que hiciera match con el nombre del sp
   try {
     let pool = await sql.connect(bdSettings);
     console.log('Ya te conectaste al server');
 
-    //probar con sp
-    let solicitud = pool.request();
+    let resultado = await pool.request()
+      .execute('spObtenerTablaOrdenada');
 
-    if (id !== null) {
-      solicitud.input('EmpleadoId', sql.Int, id);
-    } 
-    console.log('...Ejecutando SP...')
-    let resultado = await solicitud.execute('dbo.spDiccionarioEmpleados');
-
-    console.log('Datos: ', resultado.recordset);
+    console.log('Output del server: ', resultado.recordset);
+    return { succes: true };
 
   } catch (error) {
-    console.log('Error al conectar: ', error);
+    console.error('Error al ejecutar el SP: ', error);
+    return { succes: false, error: error.message}
   }
 }
 
-// Funciones UI
-
-// insertar
+// insertar empleado
 async function insertarEmpleado(nombre, salario) {
   try {
     let pool = await sql.connect(bdSettings);
@@ -74,5 +54,4 @@ async function insertarEmpleado(nombre, salario) {
   }
 }
 
-insertarEmpleado('Sahora García', 500000.00);
-mostrarTabla();
+obtenerTabla();
