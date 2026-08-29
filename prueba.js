d8e1b792508731273cc7ab17ff9fe0c7fbd1557f
mary <131309736+marypchav@users.noreply.github.com>
2026-08-29 15:26:23 -0600

    Arreglo de insertarEmpleado() para que funcione con la validación nueva del SP spInsertarEmpleado (para que no se duplique el empleado en la tabla)

--- prueba.js
+++ prueba.js
@@ -20,16 +20,16 @@ async function obtenerTabla() {
   //la renombré a "obtenerTabla" para que hiciera match con el nombre del sp
   try {
     let pool = await sql.connect(bdSettings);
-    console.log('Ya te conectaste al server');
+    console.log('Ya te conectaste al server'); //Avisa que sí se pudo entrar al server
 
-    let resultado = await pool.request()
+    let resultado = await pool.request() 
       .execute('spObtenerTablaOrdenada');
 
     console.log('Output del server: ', resultado.recordset);
     return { succes: true };
 
   } catch (error) {
-    console.error('Error al ejecutar el SP: ', error);
+    console.error('Error al ejecutar el SP', error);
     return { succes: false, error: error.message}
   }
 }
@@ -37,21 +37,28 @@ async function obtenerTabla() {
 // insertar empleado
 async function insertarEmpleado(nombre, salario) {
   try {
+
     let pool = await sql.connect(bdSettings);
-    console.log('Ya te conectaste al server');
+    console.log('Ya te conectaste al server'); //Avisa que sí se pudo entrar al server
 
     let resultado = await pool.request()
       .input('Nombre', sql.VarChar, nombre)
       .input('Salario', sql.Money, salario)
       .execute('spInsertarEmpleado');
 
-    console.log('Output del server: ', resultado.recordset[0].Mensaje);
+    console.log('Output del server: ', resultado.recordset[0].Mensaje); //Muestra el mensaje del SP "Empleado insertado correctamente"
     return { succes: true };
   
   } catch (error) {
-    console.error('Error al ejecutar el SP: ', error);
-    return { succes: false, error: error.message}
-  }
+
+    if (error.number === 51000) { //Error, nombre ya existente en la tabla
+       console.log(error.message);
+    }
+
+    console.error('Error al ejecutar el SP'); //Error general
+    return { succes: false, error: error.message }
+
+    }
 }
 
-obtenerTabla();
+insertarEmpleado('Mary Chavarría', 200000.00)
